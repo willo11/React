14704fd2b98dd24ad1201906501d0daf4d0a3d1a
willo11 <47759930+willo11@users.noreply.github.com>
2021-01-19 21:34:08 -0500

    components part2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -10,7 +18,7 @@ function App() {
           <NavbarBrand href="/">PEPE Y SANTIAGO ME LA PELAN MUCHOOOOOOO</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
 }
