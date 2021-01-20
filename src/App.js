b46486901e6c424665636ad591994391c4eb0b2d
willo11 <47759930+willo11@users.noreply.github.com>
2021-01-19 19:54:41 -0500

    configurin react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">PEPE Y SANTIAGO ME LA PELAN MUCHOOOOOOO</NavbarBrand>
+        </div>
+      </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esa tal <code>DUALIPA</code> esta sobrevalorada.
         </p>
         <a
           className="App-link"
@@ -15,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CHANGE MY MIND!
         </a>
       </header>
     </div>
